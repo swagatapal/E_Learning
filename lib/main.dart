8cfe03fe571e0a8afb27aecceb77e4fdb2737101
swagata pal <swagatapal1980@gmail.com>
2024-05-06 12:12:22 +0530

    safe area color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:e_learning/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
