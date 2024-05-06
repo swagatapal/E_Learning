7434f082c897bcc288b0727ee379207c209b1ee0
swagata pal <swagatapal1980@gmail.com>
2024-05-06 12:56:59 +0530

    home page top course

--- lib/Screens/Home/View/home_page.dart
+++ lib/Screens/Home/View/home_page.dart
@@ -32,7 +32,7 @@ class _HomePageState extends State<HomePage> {
                                 fontFamily: 'Roboto'),
                             children: <TextSpan>[
                           TextSpan(
-                            text: 'Sidara',
+                            text: ' Sidara',
                             style: TextStyle(
                                 color:
                                     Theme.of(context).colorScheme.onBackground,
@@ -92,7 +92,21 @@ class _HomePageState extends State<HomePage> {
                     isVisible: true, headingName: "Suggestions for You"),
                 const SizedBox(height: 10),
                 SizedBox(
-                  height: ScreenUtils().screenWidth(context) * 0.13,
+                  height: ScreenUtils().screenWidth(context) * 0.5,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (BuildContext context, int index) {
+                      return const SuggestionsItem();
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                HomeHeadingText(
+                    isVisible: true, headingName: "Top Courses"),
+                const SizedBox(height: 10),
+                SizedBox(
+                  height: ScreenUtils().screenWidth(context) * 0.5,
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: 5,
@@ -289,73 +303,76 @@ class SuggestionsItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: ScreenUtils().screenWidth(context) * 0.4,
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(
-          Radius.circular(15.0),
-        ),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-              // color:  Colors.blueGrey.withOpacity(0.4),
-              color: AppColors.colorSecondaryText2.withOpacity(0.2),
-              offset: const Offset(0.0, 3.0),
-              blurRadius: 8.0)
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              "assets/images/testImage1.png",
-              height: 100,
-              width: 150,
-              fit: BoxFit.cover,
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "UI/UX Design Essentials",
-                  style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.onBackground),
-                ),
-                const Text(
-                  "Tech Innovations University",
-                  style: TextStyle(
-                      fontFamily: 'Roboto',
-                      color: AppColors.primaryButtonColor,
-                      fontSize: 8,
-                      fontWeight: FontWeight.w400),
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      size: 14,
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    const Text(
-                      "4.7",
-                      style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 10,
-                          color: AppColors.primaryButtonColor),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0,right: 8),
+      child: Container(
+        width: ScreenUtils().screenWidth(context) * 0.4,
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15.0),
+          ),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                // color:  Colors.blueGrey.withOpacity(0.4),
+                color: AppColors.colorSecondaryText2.withOpacity(0.2),
+                offset: const Offset(0.0, 3.0),
+                blurRadius: 8.0)
           ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                "assets/images/testImage1.png",
+                height: 100,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "UI/UX Design Essentials",
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onBackground),
+                  ),
+                  const Text(
+                    "Tech Innovations University",
+                    style: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: AppColors.primaryButtonColor,
+                        fontSize: 8,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 14,
+                        color: Theme.of(context).colorScheme.onBackground,
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      const Text(
+                        "4.7",
+                        style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 10,
+                            color: AppColors.primaryButtonColor),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
