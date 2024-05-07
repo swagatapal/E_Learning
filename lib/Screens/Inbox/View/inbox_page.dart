5012f8547deb9dc373005114d45a2e5b72e01b8a
chirantan <basuchirantan001@gmail.com>
2024-05-07 16:32:48 +0530

    alignments fixing

--- lib/Screens/Inbox/View/inbox_page.dart
+++ lib/Screens/Inbox/View/inbox_page.dart
@@ -14,37 +14,40 @@ class _InboxPageState extends State<InboxPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Inbox",
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Roboto'),
-              ),
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                height: 30,
-                width: 30,
-                decoration: BoxDecoration(
-                  color: Colors.transparent.withOpacity(0.1),
-                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Inbox",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto'),
                 ),
-                child: Center(
-                    child: Image.asset(
-                  "assets/images/search_icon.png",
-                  height: 25,
-                  width: 15,
-                )),
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  height: 30,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent.withOpacity(0.1),
+                    borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Center(
+                      child: Image.asset(
+                    "assets/images/search_icon.png",
+                    height: 25,
+                    width: 15,
+                  )),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
