8f6f28907136eac15afbacbeaa50e23b49b3a3cf
chirantan <basuchirantan001@gmail.com>
2024-05-10 12:16:51 +0530

    edit profile page half done

--- lib/Screens/EditProfile/View/edit_profile_page.dart
+++ lib/Screens/EditProfile/View/edit_profile_page.dart
@@ -1,6 +1,9 @@
 import 'package:e_learning/Screens/EditProfile/Widgets/profile_picture.dart';
 import 'package:flutter/material.dart';
 
+import '../../../Core/Utils/Helper/app_colors.dart';
+import '../../../Core/Utils/Helper/screen_utils.dart';
+
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({super.key});
 
@@ -15,18 +18,83 @@ class _EditProfilePageState extends State<EditProfilePage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(left: 14.0, top: 14.0),
-          child: Column(
-            children: [
-              Text(
-                "Edit your Profile",
-                style: TextStyle(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    fontSize: 21.0,
-                    fontWeight: FontWeight.w500,
-                    fontFamily: 'Roboto'),
-              ),
-              // const ProfilePicture(),
-            ],
+          child: Center(
+            child: Column(
+              children: [
+                const Text(
+                  "My Profile",
+                  style: TextStyle(
+                      color: AppColors.primaryButtonColor,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto'),
+                ),
+                const ProfilePicture(),
+                Text(
+                  "Sidara Idrees",
+                  style: TextStyle(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontSize: 21.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto'),
+                ),
+                const Text(
+                  "youremail110@gmail.com",
+                  style: TextStyle(
+                      color: AppColors.primaryButtonColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: 'Roboto'),
+                ),
+                const SizedBox(height: 10.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0,right: 30.0,bottom: 20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "Edit Your Payment Method",
+                        style: TextStyle(
+                            color: AppColors.primaryButtonColor,
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Roboto'),
+                      ),
+                      InkWell(
+                        child: Image.asset(
+                          "assets/images/edit-fill.png",
+                          height: 20,
+                          width: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 1,
+                  width: ScreenUtils().screenWidth(context),
+                  color: AppColors.primaryButtonColor,
+                ),
+                const SizedBox(height: 15.0,),
+                const Text(
+                  "Progress",
+                  style: TextStyle(
+                      color: AppColors.primaryButtonColor,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Roboto'),
+                ),
+                const SizedBox(height: 8.0),
+                const Text(
+                  "Your Certificate is Ready",
+                  style: TextStyle(
+                      color: AppColors.primaryButtonColor,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w300,
+                      fontFamily: 'Roboto'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Screens/EditProfile/Widgets/profile_picture.dart
+++ lib/Screens/EditProfile/Widgets/profile_picture.dart
@@ -1,232 +1,227 @@
-// import 'dart:html';
-// import 'dart:typed_data';
-//
-// import 'package:flutter/material.dart';
-// import 'package:image_picker/image_picker.dart';
-//
-// class ProfilePicture extends StatefulWidget {
-//   const ProfilePicture({super.key});
-//
-//   @override
-//   State<ProfilePicture> createState() => _ProfilePictureState();
-// }
-//
-// class _ProfilePictureState extends State<ProfilePicture> {
-//   Uint8List? _image;
-//   // List<int> _image = [];
-//   File? selectedImage;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Stack(
-//           children: [
-//             // Profile Picture
-//             _image != null
-//                 ? CircleAvatar(
-//                     radius: MediaQuery.of(context).size.width * 0.2,
-//                     backgroundImage: MemoryImage(_image!),
-//                   )
-//                 : CircleAvatar(
-//                     radius: MediaQuery.of(context).size.width * 0.2,
-//                     backgroundColor: Colors.white,
-//                     child: CircleAvatar(
-//                       radius: MediaQuery.of(context).size.width * 0.2,
-//                       foregroundImage: const NetworkImage(
-//                         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
-//                       ),
-//                     ),
-//                   ),
-//             Positioned(
-//               bottom: 0,
-//               right: 0,
-//               child: CircleAvatar(
-//                 radius: MediaQuery.of(context).size.width * 0.07,
-//                 backgroundColor: Colors.white,
-//                 child: CircleAvatar(
-//                   radius: MediaQuery.of(context).size.width * 0.065,
-//                   backgroundColor: Colors.teal[600],
-//                   child: GestureDetector(
-//                     onTap: () {
-//                       showImagePickerOption(context);
-//                     },
-//                     child: Icon(
-//                       Icons.camera_alt,
-//                       size: MediaQuery.of(context).size.width * 0.06,
-//                       color: Colors.white,
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//   void showImagePickerOption(BuildContext context) {
-//     showModalBottomSheet(
-//       shape: const BeveledRectangleBorder(),
-//       backgroundColor: Colors.white,
-//       context: context,
-//       builder: (builder) {
-//         return Padding(
-//           padding: const EdgeInsets.all(18.0),
-//           child: SizedBox(
-//             width: MediaQuery.of(context).size.width,
-//             height: MediaQuery.of(context).size.height / 2.8,
-//             child: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Text(
-//                   "Profile photo",
-//                   style: Theme.of(context).textTheme.bodyLarge,
-//                 ),
-//                 const SizedBox(height: 40),
-//                 Padding(
-//                   padding: const EdgeInsets.only(left: 20.0),
-//                   child: Row(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       // Todo: Gallery
-//                       InkWell(
-//                         onTap: () {
-//                           _pickImageFromGallery();
-//                         },
-//                         child: Column(
-//                           children: [
-//                             CircleAvatar(
-//                               radius: MediaQuery.of(context).size.width * 0.06,
-//                               backgroundColor: Colors.white,
-//                               child: CircleAvatar(
-//                                 radius:
-//                                     MediaQuery.of(context).size.width * 0.07,
-//                                 backgroundColor: Colors.deepPurpleAccent,
-//                                 child: GestureDetector(
-//                                   onTap: () {
-//                                     //showImagePickerOption(context);
-//                                   },
-//                                   child: Icon(
-//                                     Icons.photo_size_select_actual_outlined,
-//                                     size: MediaQuery.of(context).size.width *
-//                                         0.06,
-//                                     color: Colors.white,
-//                                   ),
-//                                 ),
-//                               ),
-//                             ),
-//                             const SizedBox(height: 4),
-//                             const Text("Gallery"),
-//                           ],
-//                         ),
-//                       ),
-//                       const SizedBox(width: 50),
-//                       // Todo: Camera
-//                       InkWell(
-//                         onTap: () {
-//                           _pickImageFromCamera();
-//                         },
-//                         child: Column(
-//                           children: [
-//                             CircleAvatar(
-//                               radius: MediaQuery.of(context).size.width * 0.06,
-//                               backgroundColor: Colors.white,
-//                               child: CircleAvatar(
-//                                 radius:
-//                                     MediaQuery.of(context).size.width * 0.07,
-//                                 backgroundColor: Colors.lightBlue,
-//                                 child: GestureDetector(
-//                                   onTap: () {
-//                                     //showImagePickerOption(context);
-//                                   },
-//                                   child: Icon(
-//                                     Icons.camera_alt_rounded,
-//                                     size: MediaQuery.of(context).size.width *
-//                                         0.06,
-//                                     color: Colors.white,
-//                                   ),
-//                                 ),
-//                               ),
-//                             ),
-//                             const SizedBox(height: 4),
-//                             const Text("Camera"),
-//                           ],
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 const SizedBox(height: 10.0),
-//                 // Todo: Remove
-//                 SizedBox(
-//                   child: Padding(
-//                     padding: const EdgeInsets.only(left: 3.0, top: 16.0),
-//                     child: InkWell(
-//                       onTap: () {
-//                         //_deleteFile(selectedImage!);
-//                         selectedImage = null;
-//                         //_image=null;
-//                       },
-//                       child: Column(
-//                         children: [
-//                           CircleAvatar(
-//                             radius: MediaQuery.of(context).size.width * 0.06,
-//                             backgroundColor: Colors.white,
-//                             child: CircleAvatar(
-//                               radius: MediaQuery.of(context).size.width * 0.07,
-//                               backgroundColor: Colors.red[600],
-//                               child: GestureDetector(
-//                                 onTap: () {},
-//                                 child: Icon(
-//                                   Icons.delete,
-//                                   size:
-//                                       MediaQuery.of(context).size.width * 0.06,
-//                                   color: Colors.white,
-//                                 ),
-//                               ),
-//                             ),
-//                           ),
-//                           const SizedBox(
-//                             height: 4,
-//                           ),
-//                           const Text("Remove photo"),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-//
-//   Future _pickImageFromGallery() async {
-//     final picker = ImagePicker();
-//     final pickedImage = await picker.getImage(source: ImageSource.gallery);
-//     if (pickedImage != null) {
-//       setState(() {
-//         selectedImage = File(pickedImage.path);
-//         _image = File(pickedImage.path).readAsBytesSync();
-//       });
-//     }
-//   }
-//
-//   Future _pickImageFromCamera() async {
-//     final returnedImage =
-//         await ImagePicker().pickImage(source: ImageSource.camera);
-//     if (returnedImage == null) {
-//       return null;
-//     }
-//     setState(() {
-//       selectedImage = File(returnedImage.path);
-//       _image = File(returnedImage.path).readAsBytesSync();
-//     });
-//     Navigator.of(context).pop();
-//   }
-// }
+import 'package:cross_file_image/cross_file_image.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePicture extends StatefulWidget {
+  const ProfilePicture({super.key});
+
+  @override
+  State<ProfilePicture> createState() => _ProfilePictureState();
+}
+
+class _ProfilePictureState extends State<ProfilePicture> {
+  XFile? selectedImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Stack(
+          children: [
+            // Profile Picture
+            // _image != null
+            selectedImage != null
+                ? CircleAvatar(
+                    radius: MediaQuery.of(context).size.width * 0.2,
+                    backgroundImage: XFileImage(selectedImage!),
+                  )
+                : CircleAvatar(
+                    radius: MediaQuery.of(context).size.width * 0.2,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: MediaQuery.of(context).size.width * 0.2,
+                      foregroundImage: const NetworkImage(
+                        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+                      ),
+                    ),
+                  ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: CircleAvatar(
+                radius: MediaQuery.of(context).size.width * 0.07,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width * 0.065,
+                  backgroundColor: Colors.teal[600],
+                  child: GestureDetector(
+                    onTap: () {
+                      showImagePickerOption(context);
+                    },
+                    child: Icon(
+                      Icons.camera_alt,
+                      size: MediaQuery.of(context).size.width * 0.06,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showImagePickerOption(BuildContext context) {
+    showModalBottomSheet(
+      shape: const BeveledRectangleBorder(),
+      backgroundColor: Colors.white,
+      context: context,
+      builder: (builder) {
+        return Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Profile photo",
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 40),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Gallery
+                      InkWell(
+                        onTap: () {
+                          _pickImageFromGallery();
+                        },
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: MediaQuery.of(context).size.width * 0.06,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                radius:
+                                    MediaQuery.of(context).size.width * 0.07,
+                                backgroundColor: Colors.deepPurpleAccent,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    //showImagePickerOption(context);
+                                  },
+                                  child: Icon(
+                                    Icons.photo_size_select_actual_outlined,
+                                    size: MediaQuery.of(context).size.width *
+                                        0.06,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            const Text("Gallery"),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 50),
+                      // Camera
+                      InkWell(
+                        onTap: () {
+                          _pickImageFromCamera();
+                        },
+                        child: Column(
+                          children: [
+                            CircleAvatar(
+                              radius: MediaQuery.of(context).size.width * 0.06,
+                              backgroundColor: Colors.white,
+                              child: CircleAvatar(
+                                radius:
+                                    MediaQuery.of(context).size.width * 0.07,
+                                backgroundColor: Colors.lightBlue,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    showImagePickerOption(context);
+                                  },
+                                  child: Icon(
+                                    Icons.camera_alt_rounded,
+                                    size: MediaQuery.of(context).size.width *
+                                        0.06,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            const Text("Camera"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                // Todo: Remove
+                SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 3.0, top: 16.0),
+                    child: InkWell(
+                      onTap: () {
+                        selectedImage = null;
+                      },
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: MediaQuery.of(context).size.width * 0.06,
+                            backgroundColor: Colors.white,
+                            child: CircleAvatar(
+                              radius: MediaQuery.of(context).size.width * 0.07,
+                              backgroundColor: Colors.red[600],
+                              child: GestureDetector(
+                                onTap: () {},
+                                child: Icon(
+                                  Icons.delete,
+                                  size:
+                                      MediaQuery.of(context).size.width * 0.06,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          const Text("Remove photo"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future _pickImageFromGallery() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (returnedImage == null) {
+      return;
+    }
+    setState(() {
+      selectedImage = XFile(returnedImage.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future _pickImageFromCamera() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (returnedImage == null) {
+      return;
+    }
+    setState(() {
+      selectedImage = XFile(returnedImage.path);
+    });
+    Navigator.of(context).pop();
+  }
+}
