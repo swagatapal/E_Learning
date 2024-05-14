78cbc2a964e3d193571c17d962012a4761f06467
swagata pal <swagatapal1980@gmail.com>
2024-05-14 14:26:01 +0530

    payment page

--- lib/Screens/Payment/Widgets/overview_section.dart
+++ lib/Screens/Payment/Widgets/overview_section.dart
@@ -1,20 +1,18 @@
+import 'package:e_learning/Core/Utils/Helper/app_colors.dart';
+import 'package:e_learning/Core/Utils/Helper/screen_utils.dart';
+import 'package:e_learning/Screens/Payment/View/payment_overview.dart';
 import 'package:flutter/material.dart';
 
-import '../../../Core/Utils/Helper/app_colors.dart';
-import '../../../Core/Utils/Helper/screen_utils.dart';
-import '../../Home/Home/Widgets/category_item.dart';
-import '../View/payment_overview.dart';
-
 class OverViewSection extends StatelessWidget {
-  final ValueChanged<String>? onChanged;
   final String? hint;
   final InputBorder? inputBorder;
 
   const OverViewSection(
-      {super.key, this.onChanged, this.hint, this.inputBorder});
+      {super.key, this.hint, this.inputBorder});
 
   @override
   Widget build(BuildContext context) {
+    double wid = ScreenUtils().screenWidth(context);
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       width: ScreenUtils().screenWidth(context),
@@ -24,7 +22,7 @@ class OverViewSection extends StatelessWidget {
           Text(
             "Overview",
             style: TextStyle(
-                fontSize: 30,
+                fontSize: 20,
                 fontFamily: 'Roboto',
                 fontWeight: FontWeight.w500,
                 color: Theme.of(context).colorScheme.onBackground),
@@ -32,114 +30,198 @@ class OverViewSection extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          RichText(
-            text: TextSpan(
-              text: "Course Name:",
-              style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w700,
-                  fontSize: 18,
-                  color: Theme.of(context).colorScheme.onBackground),
-              children: const <TextSpan>[
-                TextSpan(
-                  text: '  Typography and Layout Design',
-                  style: TextStyle(
-                      color: AppColors.primaryButtonColor,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 14.0,
-                      fontFamily: 'Roboto'),
-                ),
-              ],
-            ),
-          ),
-          const CourseItem(
-            image: "assets/images/lecture.png",
-            leadingText: "50+ Lectures",
-          ),
-          const CourseItem(
-            image: "assets/images/learning.png",
-            leadingText: "4 Weeks",
-          ),
-          const CourseItem(
-            image: "assets/images/certification.png",
-            leadingText: "Online Certificate",
-          ),
-          const SizedBox(
-            height: 15,
-          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              RichText(
+                text: TextSpan(
+                  text: "Course Name:",
+                  style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
+                      fontSize: 15,
+                      color: Theme.of(context).colorScheme.onBackground),
+                  children: const <TextSpan>[
+                    TextSpan(
+                      text: '  Typography and Layout Design',
+                      style: TextStyle(
+                          color: AppColors.primaryButtonColor,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14.0,
+                          fontFamily: 'Roboto'),
+                    ),
+                  ],
+                ),
+              ),
+              const CourseItem(
+                image: "assets/images/lecture.png",
+                leadingText: "50+ Lectures",
+              ),
+              const CourseItem(
+                image: "assets/images/learning.png",
+                leadingText: "4 Weeks",
+              ),
+              const CourseItem(
+                image: "assets/images/certification.png",
+                leadingText: "Online Certificate",
+              ),
+              const SizedBox(
+                height: 15,
+              ),
               Text(
                 "Skills",
                 style: TextStyle(
                     fontFamily: 'Roboto',
                     fontWeight: FontWeight.w700,
-                    fontSize: 18,
+                    fontSize: 15,
                     color: Theme.of(context).colorScheme.onBackground),
               ),
-              const Row(
-                children: [
-                  CategoryItem(text: "Typography"),
-                  CategoryItem(text: "Layout composition"),
-                ],
+              const SizedBox(
+                height: 15,
               ),
-              const Row(
-                children: [
-                  CategoryItem(text: "Visual communication"),
-                  CategoryItem(text: "Editorial design"),
-                ],
+              Wrap(
+                children: List<Widget>.generate(
+                  5,
+                      (int idx) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Chip(
+                        label: NormalTextWidget(
+                            text: "Dart css javascript",
+                            fontSize: 13,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.white
+                        ),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(20))),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 11, horizontal: 5),
+                        backgroundColor: AppColors.primaryButtonColor,
+
+                      ),
+                    );
+                  },
+                ).toList(),
               ),
+
+              SizedBox(
+                height: wid * 0.2,
+              ),
+
             ],
           ),
-          const SizedBox(height: 30),
-          Row(
-            children: [
-              Image.asset(
-                "assets/images/money-dollar-circle-fill.png",
-                height: 30,
-                width: 30,
-              ),
-              const SizedBox(
-                width: 15,
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Total Price",
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      "35\$",
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ],
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/images/money-dollar-circle-fill.png",
+                  height: 20,
+                  width: 20,
                 ),
-              ),
-            ],
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total Price",
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 15,
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        "35\$",
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 15,
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
           Container(
+            width: ScreenUtils().screenWidth(context),
             height: 1,
             color: AppColors.primaryButtonColor,
           )
+
         ],
       ),
-      // TextField(
-      //   onChanged: (v) => onChanged!(v),
-      //   decoration: InputDecoration(hintText: hint!, border: inputBorder),
-      // ),
+
+    );
+  }
+}
+class CourseItem extends StatelessWidget {
+  final String image;
+  final String leadingText;
+
+  const CourseItem({super.key, required this.image, required this.leadingText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0,),
+      child: Row(
+        children: [
+          Image.asset(
+            image,
+            height: 15,
+            width: 15,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            width: 10.0,
+          ),
+          Text(
+            leadingText,
+            style: const TextStyle(
+                color: AppColors.colorSecondaryText2,
+                fontSize: 12.0,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Roboto'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NormalTextWidget extends StatelessWidget {
+  NormalTextWidget(
+      {super.key,
+        required this.text,
+        this.fontWeight = FontWeight.normal,
+        required this.fontSize,
+        this.maxLines = 1,
+        required this.color});
+  String text;
+  Color color;
+  FontWeight fontWeight ;
+  double fontSize = 10;
+  int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text.toString(),maxLines: maxLines,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+          color: color,
+          fontFamily: 'Roboto',
+          fontSize: fontSize,
+          fontWeight: fontWeight),
     );
   }
 }
--- lib/Screens/Payment/Widgets/select_payment_method.dart
+++ lib/Screens/Payment/Widgets/select_payment_method.dart
@@ -16,11 +16,13 @@ class _SelectPaymentMethodState extends State<SelectPaymentMethod> {
 
   @override
   Widget build(BuildContext context) {
+    double wid = ScreenUtils().screenWidth(context);
+
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       width: ScreenUtils().screenWidth(context),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             "Select Payment Method",
@@ -47,111 +49,118 @@ class _SelectPaymentMethodState extends State<SelectPaymentMethod> {
                 title: const Text(
                   "Credit / Debit Card",
                   style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.colorSecondaryText2,
-                  ),
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.colorSecondaryText2,
+                      fontSize: 13),
                 ),
               ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                      height: 40,
-                      width: 70,
-                      child: Image.asset(
-                        "assets/images/Mastercard.png",
-                        fit: BoxFit.cover,
-                      )),
-                  SizedBox(
-                      height: 40,
-                      width: 70,
-                      child: Image.asset(
-                        "assets/images/Maestro.png",
-                        fit: BoxFit.contain,
-                      )),
-                  SizedBox(
-                      height: 40,
-                      width: 70,
-                      child: Image.asset(
-                        "assets/images/PayPal.png",
-                        fit: BoxFit.cover,
-                      )),
-                  SizedBox(
-                      height: 40,
-                      width: 70,
-                      child: Image.asset(
-                        "assets/images/Visa.png",
-                        fit: BoxFit.cover,
-                      )),
-                ],
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          RadioListTile<int>(
-            value: 1,
-            activeColor: AppColors.primaryButtonColor,
-            groupValue: _selectedOption,
-            onChanged: (value) {
-              setState(() {
-                _selectedOption = value!;
-              });
-            },
-            title: const Text(
-              'JazzCash / EasyPaisa ',
-              style: TextStyle(
-                fontFamily: 'Roboto',
-                fontWeight: FontWeight.w500,
-                color: AppColors.colorSecondaryText2,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Image.asset(
-                "assets/images/money-dollar-circle-fill.png",
-                height: 30,
-                width: 30,
-              ),
-              const SizedBox(
-                width: 15,
-              ),
-              Expanded(
+              Padding(
+                padding:  EdgeInsets.only(left: wid*0.2),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      "Total Price",
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      "35\$",
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    )
+                    SizedBox(
+                        height: 30,
+                        width: 60,
+                        child: Image.asset(
+                          "assets/images/Mastercard.png",
+                          fit: BoxFit.cover,
+                        )),
+                    SizedBox(
+                        height: 30,
+                        width: 60,
+                        child: Image.asset(
+                          "assets/images/Maestro.png",
+                          fit: BoxFit.contain,
+                        )),
+                    SizedBox(
+                        height: 30,
+                        width: 60,
+                        child: Image.asset(
+                          "assets/images/PayPal.png",
+                          fit: BoxFit.cover,
+                        )),
+                    SizedBox(
+                        height: 30,
+                        width: 60,
+                        child: Image.asset(
+                          "assets/images/Visa.png",
+                          fit: BoxFit.cover,
+                        )),
                   ],
                 ),
-              )
+              ),
+              // const SizedBox(height: 10),
+              RadioListTile<int>(
+                value: 1,
+                activeColor: AppColors.primaryButtonColor,
+                groupValue: _selectedOption,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedOption = value!;
+                  });
+                },
+                title: const Text(
+                  'JazzCash / EasyPaisa ',
+                  style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.colorSecondaryText2,
+                      fontSize: 13),
+                ),
+              ),
+              SizedBox(
+                height: wid * 0.4,
+              ),
+
             ],
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15),
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/images/money-dollar-circle-fill.png",
+                  height: 20,
+                  width: 20,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total Price",
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 15,
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        "35\$",
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 15,
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
           Container(
+            width: ScreenUtils().screenWidth(context),
             height: 1,
             color: AppColors.primaryButtonColor,
           )
+
         ],
       ),
     );
