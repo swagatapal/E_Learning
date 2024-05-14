a4c986a4d9946bbfdbf23799afaf5d3565eff0cf
chirantan <basuchirantan001@gmail.com>
2024-05-14 12:04:20 +0530

    stepper

--- lib/Screens/Payment/Widgets/overview_section.dart
+++ lib/Screens/Payment/Widgets/overview_section.dart
@@ -127,7 +127,7 @@ class OverViewSection extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
+              ),
             ],
           ),
           Container(
